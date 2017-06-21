ab242b6a0f2f212e073dd6897fd5afa7d7a924ef
Wilker <wilker@MacBook-Pro-de-Wilker.local>
2017-06-21 12:05:16 -0300

    First commit project

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var gulp = require('gulp'),
+	view = require('gulp-pug'),
+	style = require('gulp-stylus'),
+    copy = require('gulp-copy'),
+    clean = require('gulp-clean'),
+    plumber = require('gulp-plumber'),
+	watch = require('gulp-watch'),
+	browserSync = require('browser-sync');
+
+// Gulp tasks
+gulp.task('browserSync',function(){
+	browserSync.init({
+		server:{
+			baseDir: 'dist'
+		}
+	});
+	// watch files on Browsersync
+	gulp.watch('src/**/*').on('change', function(event){
+        gulp.src('src/style/*.styl')
+            .pipe(plumber({
+                handleError: function(error) {
+                console.log(error);
+                this.emit('end');
+                    }
+            }))
+            .pipe(style({
+                compress: true
+            }))
+            .pipe(gulp.dest('dist/css'));
+
+        gulp.src('src/view/*.pug')
+            .pipe(view({
+                pretty: true
+            }))
+            .pipe(gulp.dest('dist/'));
+	    });
+	gulp.watch('dist/**/*').on('change', browserSync.reload);
+});
+
+// default task on gulp
+gulp.task('default',['browserSync']);
