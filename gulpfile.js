d96d8fa00cd599286d3a46813b6eef5c6135e226
Wilker <wilker@MacBook-Pro-de-Wilker.local>
2017-07-10 01:15:05 -0300

    Add index.js with tests ES6 - variables and arrow function

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,11 @@
 var gulp = require('gulp'),
 	view = require('gulp-pug'),
 	style = require('gulp-stylus'),
-    copy = require('gulp-copy'),
-    clean = require('gulp-clean'),
-    plumber = require('gulp-plumber'),
+  copy = require('gulp-copy'),
+  clean = require('gulp-clean'),
+  plumber = require('gulp-plumber'),
 	watch = require('gulp-watch'),
+  jshint = require('gulp-jshint'),
 	browserSync = require('browser-sync');
 
 // Gulp tasks
@@ -18,25 +19,30 @@ gulp.task('browserSync',function(){
 	});
 	// watch files on Browsersync
 	gulp.watch('src/**/*').on('change', function(event){
-        gulp.src('src/style/*.styl')
-            .pipe(plumber({
-                handleError: function(error) {
-                console.log(error);
-                this.emit('end');
-                    }
-            }))
-            .pipe(style({
-                compress: true
-            }))
-            .pipe(gulp.dest('dist/css'));
+    gulp.src('src/style/*.styl')
+        .pipe(plumber({
+          handleError: function(error) {
+            console.log(error);
+            this.emit('end');
+          }
+        }))
+        .pipe(style({
+          compress: true
+        }))
+        .pipe(gulp.dest('dist/css'));
 
-        gulp.src('src/view/*.pug')
-            .pipe(view({
-                pretty: true
-            }))
-            .pipe(gulp.dest('dist/'));
-	    });
-	gulp.watch('dist/**/*').on('change', browserSync.reload);
+    gulp.src('src/view/*.pug')
+        .pipe(view({
+          pretty: true
+        }))
+        .pipe(gulp.dest('dist/')); 
+
+    gulp.src('src/script/*.js')
+      .pipe(jshint())
+      .pipe(gulp.dest('dist/js'));
+    });
+
+	  gulp.watch('dist/**/*').on('change', browserSync.reload);
 });
 
 // default task on gulp
